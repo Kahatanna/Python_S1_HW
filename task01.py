b57b470f363df417443edf724971ae1c41aad102
Elena <kahatanna@gmail.com>
2022-10-04 15:56:23 +0300

    Add task01

---
+++ task01.py
@@ -0,0 +1,21 @@
+# Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+# Пример:
+
+# - 6 -> да
+# - 7 -> да
+# - 1 -> нет
+
+def day_of_week(d):
+    days = {
+        1: 'Monday',
+        2: 'Tuesday',
+        3: 'Wednesday',
+        4: 'Thursday',
+        5: 'Friday',
+        6: 'Saturday',
+        7: 'Sunday'
+    }
+    return days.get(d, 'Invalid Day of the Week')
+
+print(day_of_week(int(input('Введите номер дня недели: '))))
